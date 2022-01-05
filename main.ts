246180e3bd5c00ab2b656e1f7fa0d05e6027b06d
11kuph1mst <11kuph1mst@hft-stuttgart.de>
2022-01-05 11:26:26 +0100

    feat: Added rudimentary support for CM6 with the help of a plugin.

--- main.ts
+++ main.ts
@@ -1,42 +1,8 @@
 import { Plugin } from "obsidian";
+import {lineNumbersRelative} from "codemirror-line-numbers-relative";
 
 export default class RelativeLineNumbers extends Plugin {
-  onload() {
-    // @ts-ignore
-    const showLineNumber: Boolean = this.app.vault.getConfig("showLineNumber");
-    if (!showLineNumber) {
-      return;
-    }
-
-    this.registerCodeMirror((cm) => {
-      cm.on("cursorActivity", this.relativeLineNumbers);
-    });
-  }
-
-  onunload() {
-    this.app.workspace.iterateCodeMirrors((cm) => {
-      cm.off("cursorActivity", this.relativeLineNumbers);
-      // @ts-ignore
-      cm.setOption(
-        "lineNumberFormatter",
-        // @ts-ignore
-        CodeMirror.defaults["lineNumberFormatter"]
-      );
-    });
-  }
-
-  relativeLineNumbers(cm: CodeMirror.Editor) {
-    const current = cm.getCursor().line + 1;
-    if (cm.state.curLineNum === current) {
-      return;
-    }
-    cm.state.curLineNum = current;
-    cm.setOption("lineNumberFormatter", (line: number) => {
-      if (line === current) {
-        return String(current);
-      }
-
-      return String(Math.abs(current - line));
-    });
-  }
+  async onload() {
+		this.registerEditorExtension(lineNumbersRelative());
+	}
 }
